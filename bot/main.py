3bf1989ac5718844970c3b47b807c32aedd95599
Regi <52638916+Regy25@users.noreply.github.com>
2020-11-26 20:21:15 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,25 @@
 import os
+import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.all()
+
+komi = commands.Bot(command_prefix={'!k','!k '},intents=intents)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-@bot.event
+@komi.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print('!!!')
 
-@bot.command()
+@komi.event
+async def on_member_join(member):
+    role = discord.utils.get(member.guild.roles, name="Bot Tester")
+    await discord.Member.add_roles(member, role)
+    print(f'{member} se le dio el rol: {role}')
+
+@komi.command()
 async def ping(ctx):
-    await ctx.send("pong")
-
+    await ctx.channel.send("pong")
+    
 if __name__ == "__main__":
-    bot.run(TOKEN)
+    komi.run(TOKEN)
