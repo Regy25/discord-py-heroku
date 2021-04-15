37e7ece1b06d2fa504106f2402ec7a8e9a06ec5e
felipemeyers <55100466+felipemeyers@users.noreply.github.com>
2021-04-15 04:20:22 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -229,7 +229,7 @@ async def a2330(ctx):
 async def a24(ctx):
         await ctx.channel.send(f"A las 24 horas se juega <@&790022020925882399>, preparen sus mazos. Respondan con <:mtg:793655716862754847> para confirmar")
 
-@magicDef.command(name='commander', help="Tira un commander random")
+@komi.command(name='commander', help="Tira un commander random")
 async def commander(ctx):
         http = urllib3.PoolManager()
         pagina = randint(1,20)
